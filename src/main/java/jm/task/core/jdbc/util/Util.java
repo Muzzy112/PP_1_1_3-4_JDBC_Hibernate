2c6b38f25d276c46ece6887f914b99edf5459ca6
Muzzy112 <g112ffe@gmail.com>
2022-06-10 22:15:52 +0300

    getConnection method realization in Util class

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,30 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    // Здесь же пулл конекшенов не нужен, мне кажется?
+    private static Connection connection;
+
+    // Соединение с БД
+    public static Connection getConnection() {
+        if (connection == null){
+            String db = "pp1db";
+            String user = "root";
+            // String password = "???";
+            String password = "43133777";
+
+            try {
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + db, user, password);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
 }
